ff8eb2c308743df12a2c0d367b485a008b59181a
Ryan Douglas <ryan@luckydogdigital.com>
2015-10-29 10:21:09 -0600

    weird behavior

--- main.js
+++ main.js
@@ -1,17 +1,20 @@
 module.exports = {
 	fizzbuzztest: function(f, s, inc) {
-		var start = (s || 1);
+		var start = (s || 0);
 		var step = (inc || 1);
+		var arr = [];
 		for (var i = start; i <= f; i + step) {
-			if (i % 3 === 0 && i % 5 === 0) {
-				return "FizzBuzz";
-			} else if (i % 3 === 0) {
-				return "Fizz";
-			} else if (i % 5 === 0) {
-				return "Buzz";
+			if (i % 3 === 0 && i % 5 === 0 && i !== 0) {
+				arr.push("FizzBuzz");
+			} else if (i % 3 === 0 && i !== 0) {
+				arr.push("Fizz");
+			} else if (i % 5 === 0 && i !== 0) {
+				arr.push("Buzz");
 			} else {
-					return i;
+				arr.push(i);
 			}
 		}
+		console.log("test");
+		return arr;
 	}
 }
--- test/test.js
+++ test/test.js
@@ -3,13 +3,17 @@ var expect = require('chai').expect;
 
 describe('fizzbuzztest', function(){
 	it('should return "FizzBuzz" when i = 15', function() {
-	expect(code.fizzbuzztest(15, 15)).to.equal("FizzBuzz");
+	expect(code.fizzbuzztest(15)).to.equal("FizzBuzz");
 })
 
 	it('should return "Buzz" when i % 5 === 0', function() {
-	expect(code.fizzbuzztest(5, 5)).to.equal("Buzz");
+	expect(code.fizzbuzztest(15)).to.equal("Buzz");
 })
 	it('should return "Buzz" when i % 3 === 0', function() {
-	expect(code.fizzbuzztest(3, 3)).to.equal("Fizz");
+	expect(code.fizzbuzztest(15)).to.equal("Fizz");
 })
+	it('should return "Buzz" when i % 3 === 0', function() {
+	expect(code.fizzbuzztest(15)).to.equal(8);
+})
+
 });
